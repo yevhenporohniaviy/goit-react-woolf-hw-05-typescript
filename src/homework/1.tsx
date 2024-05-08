aa7c29143727a85ff1b1a09617caa03eb90c9d7d
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2024-05-08 17:21:12 +0300

    feat:refactor ts

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+
+interface Props {
+  children: React.ReactNode;
+  onContentEndVisible: () => void;
+}
 
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
@@ -7,8 +12,8 @@ export function Observer({ children, onContentEndVisible }: Props) {
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
+    const options: IntersectionObserverInit = {
+      rootMargin: "0px",
       threshold: 1.0,
       root: null,
     };
